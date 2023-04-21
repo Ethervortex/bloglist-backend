1cf79bff6168153733b60a5d69458bc5040a09b2
Teemu Ruokokoski <truoko@gmail.com>
2023-04-21 15:03:43 +0300

    Tehtävät 4.9 - 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,13 +2,50 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 //const logger = require('../utils/logger')
 
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+// Tehtävä 4.10 ja 4.12
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: 'Title or URL is missing' })
+  }
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0
+  })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+// Tehtävä 4.13
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+// Tehtävä 4.14
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const updateBlog = {
+    likes: body.likes
+  }
+  const updated = await Blog.findByIdAndUpdate(request.params.id, updateBlog, { new: true })
+  response.json(updated)
+})
+/*
 blogsRouter.get('/', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
     })
-})
+}) 
 
 blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
@@ -18,6 +55,6 @@ blogsRouter.post('/', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
-})
+}) */
 
 module.exports = blogsRouter
